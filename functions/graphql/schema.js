21362d4021fc29e9a9a1eb6ff0a41ce7b10eda62
Hung Nguyen <hung@redhotminute.com.au>
2019-03-15 16:37:51 +1100

    Upgrade "apollo-server-express" from version 1 to 2

--- functions/graphql/schema.js
+++ functions/graphql/schema.js
@@ -1,31 +1,57 @@
-const graphqlTools = require('graphql-tools');
+// require all dependencies to set up server
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
 const resolvers = require('./resolvers');
 
-const schema = `
-input CategoryCreateInput {
-  name: String
+// cors allows our server to accept requests from different origins
+const cors = require("cors");
+
+function configureServer() {
+
+  // invoke express to create our server
+  const app = express();
+
+  //use the cors middleware
+  app.use(cors());
+
+  // Simple graphql schema
+  const schema = gql`
+    input CategoryCreateInput {
+      name: String
+    }
+    input CategoryInput {
+      id: String!
+      name: String
+    }
+    input CategoryDeleteInput {
+      id: String!
+    }
+    type Category {
+      id: String!
+      name: String
+    }
+    type Query {
+      categories: [Category]
+    }
+    type Mutation {
+      createCategory(input: CategoryCreateInput!): Category
+      updateCategory(input: CategoryInput): Category
+      deleteCategory(input: CategoryDeleteInput): Category
+    }
+  `;
+
+  const server = new ApolloServer({
+      typeDefs: schema,
+      resolvers,
+      introspection: true,
+      playground: true
+  });
+
+  // now we take our newly instantiated ApolloServer and apply the   // previously configured express application
+  server.applyMiddleware({ app });
+      
+  // finally return the application
+  return app;
 }
-input CategoryInput {
-  id: String!
-  name: String
-}
-input CategoryDeleteInput {
-  id: String!
-}
-type Category {
-  id: String!
-  name: String
-}
-type Query {
-  categories: [Category]
-}
-type Mutation {
-  createCategory(input: CategoryCreateInput!): Category
-  updateCategory(input: CategoryInput): Category
-  deleteCategory(input: CategoryDeleteInput): Category
-}
-`;
-module.exports = graphqlTools.makeExecutableSchema({
-  typeDefs: schema,
-  resolvers
-});
+
+module.exports = configureServer;
--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const functions = require('firebase-functions');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const schemaPrinter = require('graphql/utilities/schemaPrinter');
-const schema = require('./graphql/schema');
+// require both the firebase function package to define function   // behavior and your local server config function
+const functions = require("firebase-functions");
+const configureServer = require("./graphql/schema");
 
-const app = express();
-// BODYPARSER IS ALREADY IMPLEMENTED BY FIREBASE
-app.use(cors());
-app.options('*', cors());
-app.use((req, res, next) => {
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  return next();
-});
-app.use('/graphql', graphqlExpress({ schema: schema }));
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/api/graphql' }));
-app.use('/schema', (req, res) => {
-  res.set('Content-Type', 'text/plain');
-  res.send(schemaPrinter.printSchema(schema));
-});
-exports.api = functions.https.onRequest(app);
+//initialize the server
+const server = configureServer();
+
+// create and export the api
+const api = functions.https.onRequest(server);
+module.exports = { api: api };
